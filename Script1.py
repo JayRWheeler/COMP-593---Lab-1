99f4d7b8abfb0bd59c1987587c99912dfb98d14d
JayRWheeler <Jay.Wheeler@flemingcollege.ca>
2023-01-18 13:59:00 -0500

    Updated Scripts
    - Removed Print from Script 1
    - Added Print to Script 2

--- Script1.py
+++ Script1.py
@@ -1,3 +1,2 @@
 #Print Lines from Zen of Pyhton
 print("Just kidding I missed what the lines were")
-print("Woops")
--- Script2.py
+++ Script2.py
@@ -1,3 +1,4 @@
 #Prints whatever since I missed the lines
 print("Did the chicken or the egg cross the road first?")
 print("How the hell do you expect an egg to cross the road?")
+print("Not the brightest lightbulb in the dimly lit shed, eh?")
